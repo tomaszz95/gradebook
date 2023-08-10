d6b43978f98d46eabc6684e9576fa4fd98b317f8
Tomasz Ż <tzuber95@gmail.com>
2023-08-10 15:57:41 +0200

    try configure git

--- firebase.tsx
+++ firebase.tsx
@@ -3,16 +3,16 @@ import { getAuth } from 'firebase/auth'
 let app
 
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSANGINGSENDERID,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY || 'mock_key',
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSANGINGSENDERID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
 }
 
 if (getApps().length < 1) {
-	app = initializeApp(firebaseConfig)
+    app = initializeApp(firebaseConfig)
 }
 
 const auth = getAuth(app)
